ed1d8b538ee38b9989eb33e852526472f42a72a2
ljs <artbiit@naver.com>
2024-11-15 21:00:26 +0900

    fix 돌아가게 수정중

--- src/init/index.js
+++ src/init/index.js
@@ -4,13 +4,13 @@ import { mysql } from '../db/mysql.js';
 import logger from '../utils/logger.js';
 import { connect } from '../db/redis.js';
 import { loadGameAssets } from './loadAssets.js';
-import matchMaker from '../classes/models/matchMaker.class.js';
+//import matchMaker from '../classes/models/matchMaker.class.js';
 
 const initServer = async () => {
   try {
     await Promise.all([loadGameAssets(), loadProtos(), mysql.init(), connect()]);
     logger.info('-------Data Layer initialized--------');
-    await Promise.all([matchMaker.init()]);
+    //await Promise.all([matchMaker.init()]);
     logger.info('All initialized.');
     // 다음 작업
   } catch (e) {
