89e9d4791c8f68cf02976546a13f8b835a12aea7
ljs <artbiit@naver.com>
2024-11-16 14:10:43 +0900

    fix 불필요한 로그 제거

--- src/utils/socket/messageQueue.js
+++ src/utils/socket/messageQueue.js
@@ -8,7 +8,6 @@ const queueBySocket = {};
 export const addUserQueue = (socket) => {
   //userId로 전환해야 연결이 끊겼다 들어와도 다시 큐에 정상처리 될듯?
   const id = socket.id;
-  console.log(socket.id);
   if (queueBySocket[id]) {
     //기존 큐를 비워야할지도 고민해봐야 할듯
     logger.warn(`addUserQueue. already exists queue : ${id}`);
