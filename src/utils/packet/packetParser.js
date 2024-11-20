12fdad4f50352c3e811c68e77f7056b490fa40f3
ljs <artbiit@naver.com>
2024-11-20 21:34:57 +0900

    fix 패킷파서 구조분해할당 추가

--- src/utils/packet/packetParser.js
+++ src/utils/packet/packetParser.js
@@ -10,12 +10,13 @@ export const packetParser = (packetType, payloadBuffer) => {
   // 핸들러 ID에 따라 적절한 payload 구조를 디코딩
   const packet = protoMessages[protoType];
 
-  const payload = packet.decode(payloadBuffer);
+  let payload = packet.decode(payloadBuffer);
 
   const typeName = getProtoTypeNameByHandlerId(packetType);
   const PayloadType = protoMessages[typeName];
   const expectedFields = Object.keys(PayloadType.fields);
   const actualFields = Object.keys(payload);
+  payload = { ...payload };
 
   const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
   if (missingFields.length > 0) {
