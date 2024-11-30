f9ce232926467360c84550af4d2976479f08173b
ljs <artbiit@naver.com>
2024-12-01 00:21:38 +0900

    fix 작업할 내역 작성해둠

--- src/handlers/Lobby/createLobby.handler.js
+++ src/handlers/Lobby/createLobby.handler.js
@@ -13,6 +13,14 @@ export const createLobbyHandler = async ({ payload }) => {
     gameCode: '',
     gameUrl: '아직 완성된 기능이 아닙니다.',
   };
+  /**
+   * 1. 대기중인 로비가 있는지 탐색
+   * 2. 없으면 거절
+   * 3. 있을 경우 점유 후 사용자에게 연결 전달
+   * 4. 게임 서버에 메세지 pub해서 게임 서버가 대기할 수 있도록 알림
+   * 5. 연결 정보 및 게임 코드 반환
+   *
+   */
   try {
     const now = Date.now();
     const gameInfo = {
@@ -22,8 +30,8 @@ export const createLobbyHandler = async ({ payload }) => {
       maxPlayer: 4,
       currentPlayer: 0, //세션에 연결된 수
       previousPlayer: 1, //연결 시도 중인 유저 수
-      createAt: now,
-      updateAt: now,
+      createdAt: now,
+      updatedAt: now,
     };
     for (let tryCount = 0; tryCount < 3; tryCount++) {
       const currentGameCode = nanoid();
